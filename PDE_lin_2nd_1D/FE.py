fea82f0775aa53f724024c00caf0aa806b582b6d
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-09-14 14:04:32 -0400

    finish fe, start MPS

--- PDE_lin_2nd_1D/FE.py
+++ PDE_lin_2nd_1D/FE.py
@@ -36,6 +36,70 @@ class FEFourier1DEq():
     def dim(self):
         return 2*self.P+1
     
+    @property
+    def partial1(self):
+        try:
+            return self._partial1
+        except AttributeError:
+            tmp = np.zeros((self.dim, self.dim))
+            tmp[:self.P, -self.P:] += np.diag(-(1+np.arange(self.P)))
+            tmp[-self.P:, :self.P] += np.diag((1+np.arange(self.P)))
+            self._partial1 = tmp*np.pi/self.delta
+            return self._partial1
+
+    @property
+    def partial2(self):
+        try:
+            return self._partial2
+        except AttributeError:
+            tmp = np.zeros((self.dim, self.dim))
+            tmp[:self.P, :self.P]   += np.diag(-(1+np.arange(self.P))**2)
+            tmp[-self.P:, -self.P:] += np.diag(-(1+np.arange(self.P))**2)
+            self._partial2 = tmp*(np.pi/self.delta)**2
+            return self._partial2
+    
+    @property
+    def tp_red(self):
+        try:
+            return self._tp_red
+        except AttributeError:
+            tmp = np.zeros((self.dim, self.dim, self.dim))
+            sp = np.zeros((self.P+1, self.P, self.P))
+            sn = np.zeros((self.P+1, self.P, self.P))
+            cp = np.zeros((self.P+1, self.P, self.P))
+            cn = np.zeros((self.P+1, self.P, self.P))
+
+            for p1 in range(1, self.P+1):
+                for p2 in range(1, self.P+1):
+                    pp = p1 + p2
+                    if pp <= self.P:
+                        sp[pp-1, p1-1, p2-1]    += 1
+                        cp[pp, p1-1, p2-1]      += 1
+
+                    pn = p1 - p2
+                    if pn > 0:
+                        sn[pn-1, p1-1, p2-1]    += 1
+                        cn[pn, p1-1, p2-1]      += 1
+                    elif pn == 0:
+                        cn[pn, p1-1, p2-1]      += 1
+                    else:
+                        apn = abs(pn)
+                        sn[apn-1, p1-1, p2-1]   -= 1
+                        cn[apn, p1-1, p2-1]     += 1
+
+
+            tmp[-self.P-1:, :self.P, :self.P]   += (cn-cp)/2
+            tmp[:self.P, :self.P, self.P]       += np.eye(self.P)
+            tmp[:self.P+1, :self.P, -self.P:]   += (sp+sn)/2
+            tmp[:self.P, self.P, :self.P]       += np.eye(self.P)
+            tmp[self.P, self.P, self.P]         += 1.0
+            tmp[-self.P:, self.P, -self.P:]     += np.eye(self.P)
+            tmp[:self.P+1, -self.P:, :self.P]   += (sp-sn)/2
+            tmp[-self.P:, -self.P:, self.P]     += np.eye(self.P)
+            tmp[-self.P-1:, -self.P:, -self.P:] += (cn+cp)/2
+            self._tp_red = tmp
+            return self._tp_red
+    
     def func(self, k, A):
         if len(A) != self.dim:
             raise ValueError(f'Expected {self.dim} Fourier components, but {len(A)} are given.')
@@ -51,5 +115,3 @@ class FEFourier1DEq():
                     np.sum(As*np.sin(np.pi/self.delta*np.arange(1, self.P + 1)*x.reshape((-1, 1))), axis = 1) + \
                     np.sum(Ac*np.cos(np.pi/self.delta*np.arange(1, self.P + 1)*x.reshape((-1, 1))), axis = 1)) * mask
         return genfunc
-    
-    
