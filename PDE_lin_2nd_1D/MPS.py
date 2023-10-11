58c39f66a691c51d5d3740d27fb23228bdfb6eab
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-10-10 22:22:17 -0400

    pde

--- PDE_lin_2nd_1D/MPS.py
+++ PDE_lin_2nd_1D/MPS.py
@@ -51,29 +51,65 @@ class MPSFEFourier1DEq():
         try:
             return self._U_prime
         except AttributeError:
-            self._U_prime = []
+            self._U_prime = [U.copy() for U in self.Us]
             return self._U_prime
-    
-    @property
-    def Y(self):
-        try:
-            return self._Y
-        except AttributeError:
-            self._Y = [U - self.Delta*Hc for U, Hc in zip(self.Us, self.Hcs)]
-            return self._Y
-    
-    @property
-    def h(self):
-        try:
-            return self._h
-        except AttributeError:
-            self._h = [self.Delta *\
-                       (
-                        np.einsum('ijk,k->ij', self.FE.tp_red, H0) +\
-                        np.einsum('ilk,k,lj->ij', self.FE.tp_red, H1, self.FE.partial1) +\
-                        np.einsum('ilk,k,lj->ij', self.FE.tp_red, H2, self.FE.partial2)
-                       )
-                       for H0, H1, H2 in zip(self.H0s, self.H1s, self.H2s)]
-            return self._h
         
-    def 
+    @property
+    def H_tilde(self):
+        try:
+            return self._H_tilde
+        except AttributeError:
+            self._H_tilde = self.make_H_tilde()
+            return self._H_tilde
+        
+    def multi_axes_stacking(Ts, axes):
+        tmp_shape = Ts[0].shape
+        num_axes = len(tmp_shape)
+        for T in Ts:
+            if num_axes != len(T.shape):
+                raise ValueError('All tensors must have the same ranks.')
+            tmp_shape[axes] += T.shape[axes]
+        
+        tmp = np.zeros(tmp_shape)
+        start = np.zeros(num_axes)
+        for T in Ts:
+            stop = start.copy()
+            stop[axes] += T.shape[axes]
+            slices = [slice(first, second, 1) for first, second in zip(start, stop)]
+            tmp[slices] = T
+            start = stop
+        return tmp
+        
+    def make_H_tilde(self):
+        axes_ind = [[1]] + [[0, 2]] * (self.FE.K - 1) + [[0]]
+        H_ind = [self.multi_axes_stacking([U, Hc], axes) for axes, U, Hc in zip(axes_ind, self.Us, self.Hcs)]
+
+        If = np.expand_dims(np.eye(self.FE.dim), 2)
+        Im = np.expand_dims(np.eye(self.FE.dim), (0, 3))
+        Ib = np.expand_dims(np.eye(self.FE.dim), 0)
+        Is = [If] + [Im] * (self.FE.K - 1) + [Ib]
+        axes_dep = [[2]] + [[0, 3]] * (self.FE.K - 1) + [[0]]
+        H_dep = [self.multi_axes_stacking([I, H0, H1, H2], axes) 
+                 for axes, I, H0, H1, H2 
+                 in zip(axes_dep, Is, self.H0s, self.H1s, self.H2s)]
+
+    def make_U_tilde(self):
+        pass
+
+    def make_M_L(self):
+        pass
+
+    def make_M_R(self):
+        pass
+
+    def update_U_tilde(self):
+        pass
+
+    def update_M_L(self):
+        pass
+
+    def update_M_R(self):
+        pass
+
+    def update_U_prime(self):
+        pass
